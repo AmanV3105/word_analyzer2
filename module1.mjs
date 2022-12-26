6899fd0a3a100df3e90e232f20053db7f9a647d5
AmanV3105 <108813294+AmanV3105@users.noreply.github.com>
2022-12-26 13:08:15 +0530

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,5 @@
+import ui,{d,r,s} from './module2.mjs';
+console.log(ui);
+console.log(d);
+console.log(r);
+console.log(s);
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a = "Aman";
+const d = "Dhruv";
+const r = "Rohan";
+const s = "Sam";
+
+export default a;
+export {d};
+export {r};
+export {s};
